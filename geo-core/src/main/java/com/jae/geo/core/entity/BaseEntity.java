ffb0c1c0b5cfe9303df2d882089cd5770deb9764
JAEHYUN KWON <kyzz339@naver.com>
2026-04-16 00:16:37 +0900

    gradle 설정 추가,  BaseEntity 추가

---
+++ geo-core/src/main/java/com/jae/geo/core/entity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.jae.geo.core.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column
+    private Long modifiedBy;
+
+}
